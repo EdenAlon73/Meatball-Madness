3ce3bd44c47473138121397cd7158c93dadf3143
EdenAlon73 <61311576+EdenAlon73@users.noreply.github.com>
2021-02-21 14:38:05 +0200

    Meatball mechanic

--- Assets/Scripts/Meatball.cs
+++ Assets/Scripts/Meatball.cs
@@ -1,18 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Meatball : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float xRot, yRot = 0f;
+    public Rigidbody ballRb;
+    public float rotationSpeed = 5f;
+    public float lungeForce = 30f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        transform.position = ballRb.position;
+        if (xRot < -20)
+        {
+            xRot = -20;
+        }
+
+        if (xRot > 20)
+        {
+            xRot = 20;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            xRot += Input.GetAxis("Mouse Y") * rotationSpeed;
+            yRot += Input.GetAxis("Mouse X") * rotationSpeed;
+            transform.rotation = Quaternion.Euler(xRot,yRot, 0f);
+        }
+
         
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            ballRb.velocity = transform.forward * lungeForce;
+        }
     }
 }
