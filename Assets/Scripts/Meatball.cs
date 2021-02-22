810d2700db6a5babf00518e19b840c0852343618
farkrash <62850026+farkrash@users.noreply.github.com>
2021-02-22 11:21:53 +0200

    added grounded method

--- Assets/Scripts/Meatball.cs
+++ Assets/Scripts/Meatball.cs
@@ -10,12 +10,17 @@ public class Meatball : MonoBehaviour
     public float rotationSpeed = 5f;
     public float lungeForce = 30f;
     public LineRenderer line;
-    bool isGrounded = true;
-    
+    float distToGround;
+    [SerializeField] Collider collider;
+    private void Start()
+    {
+        distToGround = collider.bounds.extents.y;
+    }
     private void Update()
     {
-        transform.position = ballRb.position;
         
+        transform.position = ballRb.position;
+
         if (xRot < -18)
         {
             xRot = -18;
@@ -28,30 +33,40 @@ public class Meatball : MonoBehaviour
 
         if (Input.GetMouseButton(0))
         {
-            
+
             xRot += Input.GetAxis("Mouse Y") * rotationSpeed;
             yRot += Input.GetAxis("Mouse X") * rotationSpeed;
-            transform.rotation = Quaternion.Euler(xRot,yRot, 0f);
+            transform.rotation = Quaternion.Euler(xRot, yRot, 0f);
             line.gameObject.SetActive(true);
             line.SetPosition(0, transform.position);
             line.SetPosition(1, transform.position + transform.forward * 10f);
         }
         
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0) && isgrounded())
         {
-            //if (isGrounded)
-            {
-                ballRb.velocity = transform.forward * lungeForce;
-                line.gameObject.SetActive(false);
-                isGrounded = false;
-            }
+            
+            
+            ballRb.velocity = transform.forward * lungeForce;
+            line.gameObject.SetActive(false);
+               
+            
         }
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        isGrounded = true;
+        print("Touch");
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            print("");
+            //isGrounded = true;
+        }
     }
 
+    private bool isgrounded()
+    {
+        return  Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
 
 }
