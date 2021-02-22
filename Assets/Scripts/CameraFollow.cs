39b2c288ce89237a7c1ae2a068483ff3c29d6e4e
EdenAlon73 <61311576+EdenAlon73@users.noreply.github.com>
2021-02-22 12:22:39 +0200

    Camera script changes for see through

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,52 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private float speed = 15f;
+
+    public Transform obstruction;
+    private float zoomSpeed = 2f;
+
+    private void Start()
+    {
+        obstruction = target;
+    }
+
     private void FixedUpdate()
     {
         transform.position = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);
         transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, speed * Time.deltaTime);
+    }
+
+    private void LateUpdate()
+    {
+        CamSeeThrough();
+    }
+
+    private void CamSeeThrough()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, target.position - transform.position, out hit, 4.5f))
+        {
+            if (hit.collider.gameObject.tag != "Player")
+            {
+                obstruction = hit.transform;
+                obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode =
+                    UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+
+                if (Vector3.Distance(obstruction.position, transform.position) >= 3f && Vector3.Distance(transform.position, target.position) >= 1.5f)
+                {
+                    transform.Translate(Vector3.forward * zoomSpeed * Time.deltaTime);
+                }
+            }
+            else
+            {
+                obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode =
+                    UnityEngine.Rendering.ShadowCastingMode.On;
+                if (Vector3.Distance(transform.position, target.position) < 4.5f)
+                {
+                    transform.Translate(Vector3.back * zoomSpeed * Time.deltaTime);
+                }
+            }
+        }
     }
 }
