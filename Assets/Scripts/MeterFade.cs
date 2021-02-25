3b394f5b6e5fc4ab5f521c1902cd58b507ac9681
farkrash <62850026+farkrash@users.noreply.github.com>
2021-02-25 14:59:36 +0200

    meter fade out

---
+++ Assets/Scripts/MeterFade.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MeterFade : MonoBehaviour
+{
+    [SerializeField]private Image arrow;
+    private Image self;
+    [SerializeField]private float arrowAlphValue;
+    private void Awake()
+    {
+        self = GetComponent<Image>();
+    }
+    private void Start()
+    {
+        arrowAlphValue = arrow.color.a;
+    }
+    private void Update()
+    {
+        var tmpColor = self.color;
+
+        arrowAlphValue = arrow.color.a;
+        tmpColor.a = arrowAlphValue;
+        self.color = tmpColor;
+    }
+}
