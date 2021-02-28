d638dc41f1b6bc11f802455626d6db85c9e04731
farkrash <62850026+farkrash@users.noreply.github.com>
2021-02-28 16:43:29 +0200

    obstacle master script

--- Assets/Scripts/ObstacleMaster.cs
+++ Assets/Scripts/ObstacleMaster.cs
@@ -9,6 +9,18 @@ public class ObstacleMaster : MonoBehaviour
     [SerializeField] private bool doImoveVertically = false;
     [SerializeField] private float rotateSpeed = 15f;
     [SerializeField] private bool doIrotateLeft = false;
+    [SerializeField] private float moveSpeed = 20f;
+    private bool dirRight = true;
+    [SerializeField] private float horizDist = 5f;
+    private float posXog;
+    private float posYog;
+    private bool dirUp = true;
+    private float verticalDist = 5f;
+    private void Start()
+    {
+        posXog = transform.position.x;
+        posYog = transform.position.y;
+    }
     private void Update()
     {
         if (doIrotate)
@@ -31,21 +43,54 @@ public class ObstacleMaster : MonoBehaviour
     {
         if (!doIrotateLeft)
         {
-            transform.Rotate(Vector3.right * (rotateSpeed * Time.deltaTime));
+            transform.Rotate(Vector3.up * (rotateSpeed * Time.deltaTime));
         }
         else
         {
-            transform.Rotate(Vector3.right * (-rotateSpeed * Time.deltaTime));
+            transform.Rotate(Vector3.up * (-rotateSpeed * Time.deltaTime));
         }
     }
 
     private void MoveHorizontally()
     {
+        if (dirRight)
+        {
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(-Vector2.right * moveSpeed * Time.deltaTime);
+        }
+
+        if(transform.position.x >= posXog + horizDist)
+        {
+            dirRight = false;
+        }
+        if(transform.position.x <= posXog - horizDist)
+        {
+            dirRight = true; 
+        }
 
     }
 
     private void MoveVertically()
     {
+        if (dirUp)
+        {
+            transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(-Vector2.up * moveSpeed * Time.deltaTime);
+        }
 
+        if (transform.position.y >= posYog + verticalDist)
+        {
+            dirUp = false;
+        }
+        if (transform.position.y <= posYog - verticalDist)
+        {
+            dirUp = true;
+        }
     }
 }
